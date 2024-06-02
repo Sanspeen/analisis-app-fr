f7de3640429d88d04e1458546a42f4ce1c650649
SanSpeen <ktns0930@gmail.com>
2024-06-02 18:01:55 -0500

    Service consume is already implemented

--- src/pages/CerosPage.jsx
+++ src/pages/CerosPage.jsx
@@ -1,16 +1,57 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'; // Importa useState
 import { Link } from 'react-router-dom';
 
 export default function CerosPage() {
+    const options = [
+        {
+            label: "Biseccion",
+            value: "biseccion",
+        },
+        {
+            label: "Falsa posicion",
+            value: "falsa-posicion",
+        },
+        {
+            label: "Newton",
+            value: "newton",
+        },
+        {
+            label: "Secante",
+            value: "secante",
+        },
+    ];
+
+    // Estado para almacenar el valor seleccionado
+    const [selectedMethod, setSelectedMethod] = useState(options[0].value);
+
+    const solveEquation = (methodToSolve) => {
+        axios({
+            method: "POST",
+            url: `http://localhost:5000/ceros-${methodToSolve}`, // Utiliza el método seleccionado como parte de la URL
+            data: {
+                function: "2*x**2 / 30 - 1",
+                lim_inferior: 1,
+                lim_superior: 10,
+                tolerancia: 1e-6
+            }
+        }).then((response) => {
+            console.log(response);
+        });
+    };
+
     return (
         <div>
             <Link to="/"><button>{"<-"}</button></Link>
             <h1>Ceros</h1>
-            <Link><button>Biseccion</button></Link>
-            <Link><button>Newton</button></Link>
-            <Link><button>Secante</button></Link>
-            <Link><button>Falsa posicion</button></Link>
-            <Link><button>Metodo grafico</button></Link>
+            {/* Usa el estado selectedMethod para controlar el valor seleccionado del select */}
+            <select value={selectedMethod} onChange={(e) => setSelectedMethod(e.target.value)}>
+                {options.map((option) => (
+                    <option key={option.value} value={option.value}>{option.label}</option>
+                ))}
+            </select>
+            {/* Pasa el método seleccionado a solveEquation */}
+            <button onClick={() => solveEquation(selectedMethod)}>Run</button>
         </div>
-    )
+    );
 }
