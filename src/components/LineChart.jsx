f12a8e6ee6eae2be1ce00a7acadd36c6961fe7d2
SanSpeen <ktns0930@gmail.com>
2024-06-02 19:15:32 -0500

    Inverted graphic pharams

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -6,14 +6,14 @@ ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement);
 
 const LineChart = ({ iteraciones, valores_iteracion }) => {
     // Crear un array de números desde 0 hasta iteraciones
-    const labels = Array.from({ length: iteraciones }, (_, index) => index);
+    const labels = valores_iteracion; 
 
     const data = {
         labels: labels,
         datasets: [
             {
                 label: 'Valores de Iteración',
-                data: valores_iteracion,
+                data:Array.from({ length: iteraciones }, (_, index) => index),
                 fill: false,
                 backgroundColor: 'rgb(75, 192, 192)',
                 borderColor: 'rgba(75, 192, 192, 0.2)',
